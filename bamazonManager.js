61c32fd8b7534183a5c0baf8d9492178f7ec3e74
Mitch Webster <mitch83083@gmail.com>
2019-07-24 21:12:06 -0400

    updated database function done

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,69 +10,9 @@ const connection = mysql.createConnection({
 
 const connectionEnd = () => {
   connection.end(err => {
-    if (err) {
-      console.log(err)
-    }
+    if (err) throw err
   })
 }
-
-const viewProducts = () => {
-  connection.query('SELECT * FROM `products`', (err, results, fields) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.table(results)
-    }
-    // connectionEnd()
-  })
-}
-
-const lowInventory = () => {
-  connection.query('SELECT * FROM `products`', (err, results, fields) => {
-    if (err) {
-      console.log(err)
-    } else {
-      const inv = results.filter(quant => quant.stockQuantity < 5)
-      console.table(inv)
-    }
-    // connectionEnd()
-  })
-}
-
-const question = () =>
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'products',
-        message: 'What would you like to do?',
-        choices: [
-          'View products for sale',
-          'View low inventory',
-          'Add to inventory',
-          'Add new product'
-        ]
-      }
-    ])
-    .then(answers => {
-      const expr = answers.products
-      switch (expr) {
-        case 'View products for sale':
-          viewProducts()
-          finished()
-          break
-        case 'View low inventory':
-          lowInventory()
-          finished()
-          break
-        case 'Add to inventory':
-          console.log('3')
-          break
-        case 'Add new product':
-          console.log('4')
-          break
-      }
-    })
 
 const finished = () => {
   inquirer
@@ -96,6 +36,162 @@ const finished = () => {
       }
     })
 }
+
+const viewProducts = () => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM `products`', (err, res) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(res)
+    })
+  })
+}
+
+const updateProducts = (total, answerId) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      'UPDATE `products` SET stockQuantity = ? WHERE itemId = ?',
+      [total, answerId.id],
+      (err, res) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(res)
+      }
+    )
+  })
+}
+
+const updatePrompts = () =>
+  inquirer.prompt([
+    {
+      type: 'number',
+      name: 'id',
+      message: 'Please enter the ID number of the item you wish to update.',
+      validate: value => !Number.isNaN(value)
+    },
+    {
+      type: 'number',
+      name: 'quantity',
+      message: 'Please enter the quantity you would like to add into stock.',
+      validate: value => !Number.isNaN(value)
+    }
+  ])
+
+const addInventory = () => {
+  viewProducts()
+    .then(data => {
+      console.table(data)
+      updatePrompts().then(answer => {
+        const total =
+          data.filter(id => id.itemId === answer.id)[0].stockQuantity +
+          answer.quantity
+        updateProducts(total, answer).then(() => {
+          viewProducts().then(data => {
+            console.table(data.filter(id => id.itemId === answer.id))
+            finished()
+          })
+        })
+      })
+    })
+    .catch(console.error)
+}
+
+const newProductRow = (columns, data) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      'INSERT INTO `products` ? VALUES ?',
+      [columns, data],
+      (err, res) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(res)
+      }
+    )
+  })
+}
+
+const newProductQuestions = () =>
+  inquirer.prompt([
+    {
+      name: 'name',
+      message: 'Please enter the name of the product you would like to add.'
+      // validate:
+    },
+    {
+      name: 'department',
+      message: 'What department would the product be sold in?'
+      // validate:
+    },
+    {
+      name: 'price',
+      message: 'What is the price of the product?',
+      validate: value => !Number.isNaN(value)
+    },
+    {
+      name: 'quantity',
+      message: 'How many of them do we have in stock?',
+      validate: value => !Number.isNaN(value)
+    }
+  ])
+
+const newProduct = () => {
+  const columns = ['productName', 'departmentName', 'price', 'stockQuantity']
+  newProductQuestions().then(answers => {
+    const data = [
+      answers.name,
+      answers.department,
+      answers.price,
+      answers.quantity
+    ]
+    newProductRow(columns, data).then(() => viewProducts())
+  })
+}
+
+const question = () =>
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'products',
+        message: 'What would you like to do?',
+        choices: [
+          'View products for sale',
+          'View low inventory',
+          'Add to inventory',
+          'Add new product'
+        ]
+      }
+    ])
+    .then(answers => {
+      const expr = answers.products
+      switch (expr) {
+        case 'View products for sale':
+          viewProducts()
+            .then(data => {
+              console.table(data)
+              finished()
+            })
+            .catch(console.error)
+          break
+        case 'View low inventory':
+          viewProducts()
+            .then(data => {
+              console.table(data.filter(quant => quant.stockQuantity < 5))
+              finished()
+            })
+            .catch(console.error)
+          break
+        case 'Add to inventory':
+          addInventory()
+          break
+        case 'Add new product':
+          newProduct()
+          break
+      }
+    })
 
 question()
 // finished()
